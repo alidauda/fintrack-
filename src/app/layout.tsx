704cd70ac376b353d041e7bfe463f01c030131ce
alidauda <alidauda14@gmail.com>
2025-08-07 12:55:53 +0100

    Add Open Graph and Twitter metadata to enhance SEO and social sharing for FinTrack

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,9 +1,32 @@
 import ClientLayout from "@/components/layout/ClientLayout";
 import { Metadata } from "next";
+import "./globals.css";
 
 export const metadata: Metadata = {
   title: "FinTrack",
   description: "FinTrack is a platform for managing your finances",
+  openGraph: {
+    title: "FinTrack",
+    description: "FinTrack is a platform for managing your finances",
+    url: "https://fintrack-rouge.vercel.app/", 
+    siteName: "FinTrack",
+    images: [
+      {
+        url: "/logo.png",
+        width: 1200,
+        height: 630,
+        alt: "FinTrack Logo",
+      },
+    ],
+    locale: "en_US",
+    type: "website",
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: "FinTrack",
+    description: "FinTrack is a platform for managing your finances",
+    images: ["/logo.png"],
+  },
 };
 
 export default function RootLayout({
